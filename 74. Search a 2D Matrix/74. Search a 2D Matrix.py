4ce7c14d340fcfd2016789ce04aa02124c335460
Betselot Getnet Semaw <betselotgetnet2227@gmail.com>
2026-03-22 16:05:40 -0400

    Update solution: 74. Search a 2D Matrix

--- 74. Search a 2D Matrix/74. Search a 2D Matrix.py
+++ 74. Search a 2D Matrix/74. Search a 2D Matrix.py
@@ -1,21 +1,37 @@
 #
 # Problem: 74. Search a 2D Matrix
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/search-a-2d-matrix/submissions/1955168982/
+# Link: https://leetcode.com/problems/search-a-2d-matrix/submissions/1956107213/
 # Language: python3
-# Date: 2026-03-21
+# Date: 2026-03-22
 
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m, n = len(matrix), len(matrix[0])
-        r, c = 0, n - 1
+        left, right = 0, len(matrix)-1
+        
 
-        while r < m and c >= 0:
-            if matrix[r][c] > target:
-                c -= 1
-            elif matrix[r][c] < target:
-                r += 1
+        while left <= right:
+            mid = (left + right ) // 2
+            left_m, right_m = 0, len(matrix[0])-1
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                while left_m <= right_m:
+                    mid_m = ( left_m + right_m ) // 2
+                    if matrix[mid][mid_m] > target:
+                        right_m = mid_m - 1
+                    elif matrix[mid][mid_m] < target:
+                        left_m = mid_m + 1
+                    else:
+                        return True
+                return False
+            elif matrix[mid][-1] < target:
+                left = mid + 1
+            
             else:
-                return True
+                right = mid - 1
+
+        
+
         return False
+
+        
