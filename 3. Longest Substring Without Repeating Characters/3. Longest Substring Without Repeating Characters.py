4d9ae9a7e986f62c7c0f230cd0d625e129a71d50
Betselot Getnet Semaw <betselotgetnet2227@gmail.com>
2026-03-28 18:57:05 -0400

    Update solution: 3. Longest Substring Without Repeating Characters

--- 3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters/3. Longest Substring Without Repeating Characters.py
@@ -1,9 +1,9 @@
 #
 # Problem: 3. Longest Substring Without Repeating Characters
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1961307730/
+# Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1962259980/
 # Language: python3
-# Date: 2026-03-27
+# Date: 2026-03-28
 
 
 class Solution:
@@ -11,11 +11,10 @@ class Solution:
         charSet = set()
         l = 0
         res = 0
-
         for r in range(len(s)):
             while s[r] in charSet:
                 charSet.remove(s[l])
                 l += 1
             charSet.add(s[r])
-            res = max(res, r - l + 1)
+            res = max(res, r-l+1)
         return res
