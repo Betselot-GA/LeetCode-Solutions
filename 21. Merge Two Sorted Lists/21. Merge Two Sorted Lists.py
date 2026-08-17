616d175f7ed169a1951c4bb51a5d8265719d8722
Betselot Getnet Semaw <betselotgetnet2227@gmail.com>
2026-08-17 18:44:44 -0400

    Update solution: 21. Merge Two Sorted Lists

--- 21. Merge Two Sorted Lists/21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists/21. Merge Two Sorted Lists.py
@@ -1,9 +1,9 @@
 #
 # Problem: 21. Merge Two Sorted Lists
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/merge-two-sorted-lists/submissions/1994851440/
+# Link: https://leetcode.com/problems/merge-two-sorted-lists/submissions/2110709679/
 # Language: python3
-# Date: 2026-05-04
+# Date: 2026-08-17
 
 
 # Definition for singly-linked list.
@@ -13,22 +13,30 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        tail = dummy
+        newList = ListNode()
+        curr = newList
 
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
+        while list1 or list2:
+            if list1 and list2:
+                if list1.val < list2.val:
+                    curr.next = list1
+                    list1 = list1.next
+                elif list1.val > list2.val:
+                    curr.next = list2
+                    list2 = list2.next
+                else:
+                    curr.next = list1
+                    list1 = list1.next
+            elif not list1:
+                curr.next = list2
+                return newList.next
             else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next
+                curr.next = list1
+                return newList.next
+            curr = curr.next
+        return newList.next
         
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
-        
-        return dummy.next
-        
+            
+
+
+
