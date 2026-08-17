232850cbca67ba80b3575f7c584e5bb28f9859f2
Betselot Getnet Semaw <betselotgetnet2227@gmail.com>
2026-08-17 17:06:28 -0400

    Update solution: 206. Reverse Linked List

--- 206. Reverse Linked List/206. Reverse Linked List.py
+++ 206. Reverse Linked List/206. Reverse Linked List.py
@@ -1,9 +1,9 @@
 #
 # Problem: 206. Reverse Linked List
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/reverse-linked-list/submissions/1994825692/
+# Link: https://leetcode.com/problems/reverse-linked-list/submissions/2110675151/
 # Language: python3
-# Date: 2026-05-04
+# Date: 2026-08-17
 
 
 # Definition for singly-linked list.
@@ -13,11 +13,10 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
+        prev,curr = None, head
         while curr:
-            nxt = curr.next
+            temp = curr.next
             curr.next = prev
-            prev = curr
-            curr = nxt
+            prev,curr = curr, temp
         return prev
         
